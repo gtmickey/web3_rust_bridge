fd74042246e841439a3f1ab521fb92e8e79c2446
gtmickey <gtmickey439@gmail.com>
2024-04-29 14:31:52 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web3_rust_bridge
+
+一个封装rust代码的接口并提供给dart层用的中间层
---
+++ android/app/src/main/kotlin/com/example/web3_rust_bridge/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.web3_rust_bridge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
