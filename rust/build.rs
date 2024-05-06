a1e501eb40b749dd7f00c19e0a10cc5b2f333450
gtmickey <gtmickey439@gmail.com>
2024-05-06 09:27:46 +0800

    update

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,7 @@
 fn main() {
 
 
-    // 如果在macos上运行时，一下全关闭
+    // 如果在macos上运行时，以下全关闭
 
     // 设置对应的库
     //  编译 ios模拟器时 打开这个注释 cargo lipo --targets aarch64-apple-ios-sim --release
@@ -9,5 +9,5 @@ fn main() {
 
 
     //  编译 ios真机时 打开这个注释 cargo lipo --targets aarch64-apple-ios --release
-    // println!("cargo:rustc-link-search=native=./ios_curl/artifacts-iphoneos/lib");
+    println!("cargo:rustc-link-search=native=./ios_curl/artifacts-iphoneos/lib");
 }
