34480037a796a2b0e84a862c4250e51120cc41ec
gtmickey <gtmickey439@gmail.com>
2024-05-29 09:10:18 +0800

    update to flutter_rust_bridge_codegen 2.0.0-dev.36

--- rust/build.rs
+++ rust/build.rs
@@ -3,6 +3,7 @@ fn main() {
 
     // 如果在macos上运行时，以下全关闭
 
+    // 编译或运行ios时，按对应平台打开
     // 设置对应的库
     //  编译 ios模拟器时 打开这个注释 cargo lipo --targets aarch64-apple-ios-sim --release
     // println!("cargo:rustc-link-search=native=./ios_curl/artifacts-iphonesimulator/lib");
